94460f683285e1e17a9fb73a7151aa9dda8737be
AJ <95362658+TiredAJ@users.noreply.github.com>
2024-12-03 20:28:02 +0000

    init

---
+++ Src/CheekyWrapped/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CheekyWrapped;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Src/CheekyWrapped/Scrobble.cs
@@ -0,0 +1,4 @@
+namespace CheekyWrapped
+{
+    public record Scrobble();
+}
